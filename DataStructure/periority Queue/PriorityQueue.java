7967235e39e62c54406e5010ad7b2f4a6576ca78
priyaraj97 <51694650+priyaraj97@users.noreply.github.com>
2021-07-15 18:27:13 +0530

    Add files via upload

---
+++ DataStructure/periority Queue/PriorityQueue.java
@@ -0,0 +1,70 @@
+package periorityqueue;
+
+public class PriorityQueue 
+{
+	    private Task[] heap; 
+	    private int heapSize, capacity;
+	        public PriorityQueue(int capacity)
+	   {    
+	        this.capacity = capacity + 1;
+	        heap = new Task[this.capacity];
+	        heapSize = 0;
+	    }
+	    public void clear()
+	    {
+	        heap = new Task[capacity];
+	        heapSize = 0;
+	    }
+	 public boolean isEmpty()
+	    {
+	        return heapSize == 0;
+	    }
+	    public boolean isFull()
+	   {
+	      return heapSize == capacity - 1;
+	    }
+	      public int size()
+	    {
+	        return heapSize;
+	    }
+	    public void insert(String job, int priority)
+	   {
+	        Task newJob = new Task(job, priority);
+	        heap[++heapSize] = newJob;
+	        int pos = heapSize;
+	        while (pos != 1 && newJob.priority > heap[pos/2].priority)
+	        {
+	            heap[pos] = heap[pos/2];
+	            pos /=2;
+	        }
+	       heap[pos] = newJob;    
+	    }
+	    public Task remove()
+	    {
+	        int parent, child;
+	        Task item, temp;
+	        if (isEmpty() )
+	        {
+	            System.out.println("Heap is empty");
+	            return null;
+	       }
+	       item = heap[1];
+	        temp = heap[heapSize--];
+	       parent = 1;
+	       child = 2;
+	       while (child <= heapSize)
+	       {
+	            if (child < heapSize && heap[child].priority < heap[child + 1].priority)
+	                child++;
+	           if (temp.priority >= heap[child].priority)
+	                break;
+	            heap[parent] = heap[child];
+	            parent = child;
+	            child *= 2;
+	        }
+	        heap[parent] = temp;
+	       return item;
+	    }
+	}
+
+
