a23bf68fb12e8b638a87869f413525ab17442f2b
priyaraj97 <51694650+priyaraj97@users.noreply.github.com>
2021-08-16 20:54:12 +0530

    Add files via upload

---
+++ Microsoft department service/Department.java
@@ -0,0 +1,47 @@
+package com.dailycodebuffer.department.entity;
+
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int departmentId;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+    public int getDepartmentId() {
+		return departmentId;
+	}
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+	public String getDepartmentName() {
+		return departmentName;
+	}
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+	public String getDepartmentAddress() {
+		return departmentAddress;
+	}
+	public void setDepartmentAddress(String departmentAddress) {
+		this.departmentAddress = departmentAddress;
+	}
+	public String getDepartmentCode() {
+		return departmentCode;
+	}
+	public void setDepartmentCode(String departmentCode) {
+		this.departmentCode = departmentCode;
+	}
+	
+}
