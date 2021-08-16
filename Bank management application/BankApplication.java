9fbc0ace033e8fbf8dcd2a46105f99fb2be8a04e
priyaraj97 <51694650+priyaraj97@users.noreply.github.com>
2021-08-16 20:49:19 +0530

    Add files via upload

--- Bank management application/BankApplication.java
+++ Bank management application/BankApplication.java
@@ -22,6 +22,7 @@ import com.adem.entities.SavingsAccount;
 import com.adem.entities.WithdrawalOperation;
 import com.adem.service.IBankService;
 
+@SuppressWarnings("unused")
 @SpringBootApplication 
 public class BankApplication  { // implements CommandLineRunner
 
@@ -40,29 +41,5 @@ public class BankApplication  { // implements CommandLineRunner
 
 	}
 	
-	/*@Override
-	public void run(String... arg0) throws Exception {
 
-		Customer cust1 = customerRepository.save(new Customer("adem", "adem@gmail.com"));
-		Customer cust2 = customerRepository.save(new Customer("asma", "asma@gmail.com"));
-
-		Account currAcc = accountRepository.save(new CurrentAccount("currAcc", new Date(), 9000, cust1, 6000));
-		Account savAcc = accountRepository.save(new SavingsAccount("savAcc", new Date(), 6000, cust2, 5.5));
-
-		Operation paymentOpcurrAcc = operationRepository.save(new PaymentOperation(new Date(), 2000, currAcc));
-		Operation widhdrawelOpcurrAcc = operationRepository.save(new WithdrawalOperation(new Date(), 1000, currAcc));
-		Operation paymentOp1currAcc = operationRepository.save(new PaymentOperation(new Date(), 2000, currAcc));
-		Operation widhdrawelOp1currAcc = operationRepository.save(new WithdrawalOperation(new Date(), 1000, currAcc));
-		Operation paymentOp2currAcc = operationRepository.save(new PaymentOperation(new Date(), 5000, currAcc));
-		Operation widhdrawelOp2currAcc = operationRepository.save(new WithdrawalOperation(new Date(), 1000, currAcc));
-		Operation paymentOp3currAcc = operationRepository.save(new PaymentOperation(new Date(), 8741, currAcc));
-		Operation widhdrawelOp3currAcc = operationRepository.save(new WithdrawalOperation(new Date(), 2000, currAcc));
-		Operation paymentOp4currAcc= operationRepository.save(new PaymentOperation(new Date(), 6587, currAcc));
-		Operation widhdrawelOp4currAcc = operationRepository.save(new WithdrawalOperation(new Date(), 2547, currAcc));
-
-		Operation paymentOpsavAcc = operationRepository.save(new PaymentOperation(new Date(), 1000, savAcc));
-		Operation widhdrawelOpsavAcc = operationRepository.save(new WithdrawalOperation(new Date(), 2000, savAcc));
-
-		iBankService.payToAccount(currAcc.getAccountId(), 99999999);
-	} */
 }
